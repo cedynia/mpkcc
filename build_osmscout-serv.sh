e4ee71c391e4b18788818d73140993bec177615a
Pawel <cedynia@adres.pl>
2019-12-26 10:50:52 +0100

    testy kyoto fail

--- build_osmscout-serv.sh
+++ build_osmscout-serv.sh
@@ -2,105 +2,105 @@
 
 ##sudo apt-get install dh-autoreconf
 
-#firstly compile the mapnik library with their dependencies
-#global_var="$(dirname "$0")"
-#. "$global_var/script_dev.sh"
-#
-# global_var="$(dirname "$0")"
-# . "$global_var/global_fun.sh"
-#
-# global_var="$(dirname "$0")"
-# . "$global_var/osmscout-server_var.sh"
+firstly compile the mapnik library with their dependencies
+global_var="$(dirname "$0")"
+. "$global_var/script_dev.sh"
+
+global_var="$(dirname "$0")"
+. "$global_var/global_fun.sh"
+
+global_var="$(dirname "$0")"
+. "$global_var/osmscout-server_var.sh"
 
 
-# export CC_COMPILER="/home/pawel/mapnik4android/android-toolchain-API23-x86_64/bin/clang"
-# export CXX_COMPILER="/home/pawel/mapnik4android/android-toolchain-API23-x86_64/bin/clang++"
+export CC_COMPILER="/home/pawel/mapnik4android/android-toolchain-API23-x86_64/bin/clang"
+export CXX_COMPILER="/home/pawel/mapnik4android/android-toolchain-API23-x86_64/bin/clang++"
 
 
 #now try to build osmscout-server dependencies...
 ####libpostal##############################################################
-cd "$MYPWD"
-cdIntoGitRepo "$LIBPOSTAL_GIT" "$LIBPOSTAL_FOLDER" "$LIBPOSTAL_BUILD"
-
-./bootstrap.sh
-
-CXXFLAGS="-fPIC" CFLAGS="-fPIC"  ./configure  \
-        --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBPOSTAL_BUILD" \
-        CC=$CC_COMPILER \
-        CXX=$CXX_COMPILER \
-        --host=arm-linux \
-        --enable-static \
-        --disable-shared \
-        PKG_CONFIG="" \
-        --disable-sse2 \
-        --datadir=/usr/local/libpostal/data \
-        --disable-data-download
-
-make install -j$NPROC
-
-###marisa-trie#################################################################
-cd "$MYPWD"
-#mkdir "$SCOUT_OUT_FOLDER/libmarisa"
-#git clone https://github.com/s-yata/marisa-trie.git
-#cd "$MYPWD/marisa-trie"
-cdIntoGitRepo "$LIBMARISA_GIT" "$LIBMARISA_FOLDER" "$LIBMARISA_BUILD"
-autoreconf -i
-
-CXXFLAGS="-fPIC" CFLAGS="-fPIC" ./configure \
-      --prefix="$MYPWD/$OUTPUT_FOLDER/libmarisa" \
-      CC=$CC_COMPILER \
-      CXX=$CXX_COMPILER \
-      --host=arm-linux  \
-      --enable-static \
-      --disable-shared  \
-      PKG_CONFIG=""
-
-make install -j$NPROC
-
-###libosmscout##############################################################
-cd "$MYPWD"
-#mkdir "$SCOUT_OUT_FOLDER/libosmscout"
-#git clone --recurse-submodule https://github.com/rinigus/libosmscout.git
-#cd "$MYPWD/libosmscout/libosmscout"
-
-cdIntoGitRepo "$LIBOSMSCOUT_GIT" "$LIBOSMSCOUT_FOLDER" "$LIBOSMSCOUT_BUILD"
-
-patch src/osmscout/util/FileScanner.cpp < $MYPWD/patches/libosmscout_FileScanner.patch
-
-./autogen.sh
-CXXFLAGS="-fPIC" CFLAGS="-fPIC"  ./configure  \
-        --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER" \
-        CC=$CC_COMPILER \
-        CXX=$CXX_COMPILER \
-        --host=arm-linux \
-        --enable-static \
-        --disable-shared \
-        MARISA_LIBS="$MYPWD/$OUTPUT_FOLDER/libmarisa/lib" \
-        MARISA_CFLAGS=-I"$MYPWD/$OUTPUT_FOLDER/libmarisa/include" \
-        PKG_CONFIG=""
-
-make install -j$NPROC
-cp -rf $MYPWD/$BUILD_FOLDER/$LIBOSMSCOUT_FOLDER/$LIBOSMSCOUT_BUILD/include/osmscout/* $MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/include/osmscout/
-
-###libosmscout-map##############################################################
-#cd "$MYPWD"
-#cd "$MYPWD/libosmscout/libosmscout-map"
-
-cdIntoGitRepo "$LIBOSMSCOUT_GIT" "$LIBOSMSCOUT_FOLDER" "$LIBOSMSCOUTMAP_BUILD"
-
-./autogen.sh
-CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure  \
-        --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER" \
-        CC=$CC_COMPILER \
-        CXX=$CXX_COMPILER \
-        --host=arm-linux  \
-        --enable-static \
-        --disable-shared  \
-        PKG_CONFIG="" \
-        LIBOSMSCOUT_LIBS="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/lib"  \
-        LIBOSMSCOUT_CFLAGS=-I"$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/include"
-
-make install -j$NPROC
+# cd "$MYPWD"
+# cdIntoGitRepo "$LIBPOSTAL_GIT" "$LIBPOSTAL_FOLDER" "$LIBPOSTAL_BUILD"
+#
+# ./bootstrap.sh
+#
+# CXXFLAGS="-fPIC" CFLAGS="-fPIC"  ./configure  \
+#         --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBPOSTAL_BUILD" \
+#         CC=$CC_COMPILER \
+#         CXX=$CXX_COMPILER \
+#         --host=arm-linux \
+#         --enable-static \
+#         --disable-shared \
+#         PKG_CONFIG="" \
+#         --disable-sse2 \
+#         --datadir=/usr/local/libpostal/data \
+#         --disable-data-download
+#
+# make install -j$NPROC
+#
+# ###marisa-trie#################################################################
+# cd "$MYPWD"
+# #mkdir "$SCOUT_OUT_FOLDER/libmarisa"
+# #git clone https://github.com/s-yata/marisa-trie.git
+# #cd "$MYPWD/marisa-trie"
+# cdIntoGitRepo "$LIBMARISA_GIT" "$LIBMARISA_FOLDER" "$LIBMARISA_BUILD"
+# autoreconf -i
+#
+# CXXFLAGS="-fPIC" CFLAGS="-fPIC" ./configure \
+#       --prefix="$MYPWD/$OUTPUT_FOLDER/libmarisa" \
+#       CC=$CC_COMPILER \
+#       CXX=$CXX_COMPILER \
+#       --host=arm-linux  \
+#       --enable-static \
+#       --disable-shared  \
+#       PKG_CONFIG=""
+#
+# make install -j$NPROC
+#
+# ###libosmscout##############################################################
+# cd "$MYPWD"
+# #mkdir "$SCOUT_OUT_FOLDER/libosmscout"
+# #git clone --recurse-submodule https://github.com/rinigus/libosmscout.git
+# #cd "$MYPWD/libosmscout/libosmscout"
+#
+# cdIntoGitRepo "$LIBOSMSCOUT_GIT" "$LIBOSMSCOUT_FOLDER" "$LIBOSMSCOUT_BUILD"
+#
+# patch src/osmscout/util/FileScanner.cpp < $MYPWD/patches/libosmscout_FileScanner.patch
+#
+# ./autogen.sh
+# CXXFLAGS="-fPIC" CFLAGS="-fPIC"  ./configure  \
+#         --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER" \
+#         CC=$CC_COMPILER \
+#         CXX=$CXX_COMPILER \
+#         --host=arm-linux \
+#         --enable-static \
+#         --disable-shared \
+#         MARISA_LIBS="$MYPWD/$OUTPUT_FOLDER/libmarisa/lib" \
+#         MARISA_CFLAGS=-I"$MYPWD/$OUTPUT_FOLDER/libmarisa/include" \
+#         PKG_CONFIG=""
+#
+# make install -j$NPROC
+# cp -rf $MYPWD/$BUILD_FOLDER/$LIBOSMSCOUT_FOLDER/$LIBOSMSCOUT_BUILD/include/osmscout/* $MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/include/osmscout/
+#
+# ###libosmscout-map##############################################################
+# #cd "$MYPWD"
+# #cd "$MYPWD/libosmscout/libosmscout-map"
+#
+# cdIntoGitRepo "$LIBOSMSCOUT_GIT" "$LIBOSMSCOUT_FOLDER" "$LIBOSMSCOUTMAP_BUILD"
+#
+# ./autogen.sh
+# CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure  \
+#         --prefix="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER" \
+#         CC=$CC_COMPILER \
+#         CXX=$CXX_COMPILER \
+#         --host=arm-linux  \
+#         --enable-static \
+#         --disable-shared  \
+#         PKG_CONFIG="" \
+#         LIBOSMSCOUT_LIBS="$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/lib"  \
+#         LIBOSMSCOUT_CFLAGS=-I"$MYPWD/$OUTPUT_FOLDER/$LIBOSMSCOUT_FOLDER/include"
+#
+# make install -j$NPROC
 
 ###kyoto########################################################################
 cd "$MYPWD"
